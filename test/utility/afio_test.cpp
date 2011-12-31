f7292209f60f2f79c11c4d99f8eb2f84d0357ea9
Pooya Salehi <p00ya00@yahoo.com>
2011-12-31 17:18:22 +0330

    afio test

--- test/utility/afio_test.cpp
+++ test/utility/afio_test.cpp
@@ -5,14 +5,19 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cstring>
 #include <boost/thread.hpp>
 #include <boost/asio.hpp>
+
+using namespace sf::afio;
+using namespace boost::system;
+namespace asio_error = boost::asio::error;
 
 //Sync access to std::cout
 boost::mutex coutMutex;
 
 //Enable/disable logging diagnostics information
-#define LOG_INFO false
+#define LOG_INFO true
 
 std::string datasetPath()
 {
@@ -50,6 +55,33 @@ void assertRead(const boost::system::error_code &error, std::size_t transferred,
     }
 }
 
+void assertWrite(const boost::system::error_code &error, std::size_t transferred,
+                 std::size_t expectedTransferred, const boost::system::error_code &expectedError,
+                 bool printDiagnostics)
+{
+    BOOST_REQUIRE(error == expectedError);
+    BOOST_REQUIRE_EQUAL(transferred, expectedTransferred);
+
+    if(printDiagnostics && LOG_INFO)
+    {
+        boost::lock_guard<boost::mutex> lock(coutMutex);
+        std::clog << "\n\n\nError: " << error << std::endl;
+        std::clog << "Transferred: " << transferred << std::endl;
+        std::clog << "Expected error: " << expectedError << std::endl;
+        std::clog << "Expected transfer: " << expectedTransferred << std::endl;
+    }
+}
+
+void handleWrite(const boost::system::error_code &error, std::size_t transferred)
+{
+    if(LOG_INFO)
+    {
+        boost::lock_guard<boost::mutex> lock(coutMutex);
+        std::clog << "\n\n\nError: " << error << std::endl;
+        std::clog << "Transferred: " << transferred << std::endl;
+    }
+}
+
 void pause(boost::uint16_t sec)
 {
     static boost::asio::io_service ios;
@@ -62,8 +94,6 @@ void pause(boost::uint16_t sec)
 ///////////////////////////////////////////////////////////////////////////////
 
 BOOST_AUTO_TEST_SUITE(afio_unit_test)
-
-using namespace sf::afio;
 
 BOOST_AUTO_TEST_CASE(is_open)
 {
@@ -89,15 +119,19 @@ BOOST_AUTO_TEST_CASE(path)
     BOOST_REQUIRE_EQUAL(tempFile.path(), datasetPath() + "temp.bin");
 }
 
+BOOST_AUTO_TEST_CASE(cancel_invalid_file)
+{
+    AsyncFileIn invalidFile(datasetPath() + "invalid_file.pdf", READ_M);
+
+    BOOST_REQUIRE(!invalidFile.isOpen());
+    BOOST_REQUIRE_THROW(invalidFile.cancel(), error::CancellationFailure);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 ///////////////////////////////////////////////////////////////////////////////
 
 BOOST_AUTO_TEST_SUITE(afio_async_read_test)
-
-using namespace sf::afio;
-using namespace boost::system;
-namespace asio_error = boost::asio::error;
 
 BOOST_AUTO_TEST_CASE(successful)
 {
@@ -195,10 +229,62 @@ BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE(afio_async_write_test)
 
 BOOST_AUTO_TEST_CASE(successful)
-{}
+{
+    std::string buf =  "In order to investigate nature Beckett exposes in his "
+                       "work the frames within which the two events (of being "
+                       "and of signification and symbolization) are produced."
+                       " \n\nFor Beckett, nature is (dis)placed between intervals"
+                       " when the faculty of reason is asleep, and when it is "
+                       "active. Nature is apprehended differently, at distance "
+                       "from reason (metaphorically) and from within reason "
+                       "(metonymically).";
+
+	AsyncFileOut outputFile(datasetPath() + "write_test.txt", WRITE_M);
+    std::size_t writeSize = buf.size();
+    error_code successErrorCode; //default constructor creates 'success' error code
+	
+    BOOST_REQUIRE(outputFile.isOpen());
+    outputFile.asyncWrite(boost::asio::buffer(buf),
+	                      boost::bind(&assertWrite,
+	                                  boost::asio::placeholders::error, 
+	                                  boost::asio::placeholders::bytes_transferred, 
+	                                  writeSize, successErrorCode, true));
+    
+    std::vector<char> buf2(20, '$');
+    AsyncFileInOut bufContent(datasetPath() + "buf_content.bin", READ_WRITE_M);
+    writeSize = 10;
+    const std::vector<char> &bufRef = buf2;
+
+    BOOST_REQUIRE(bufContent.isOpen());
+    bufContent.asyncWrite(boost::asio::buffer(bufRef, writeSize),
+                          boost::bind(&assertWrite,
+                                      boost::asio::placeholders::error, 
+	                                  boost::asio::placeholders::bytes_transferred, 
+	                                  writeSize, successErrorCode, true), 
+                          writeSize, 5);
+    
+    pause(1);
+    outputFile.close();
+    bufContent.close();
+}
 
 BOOST_AUTO_TEST_CASE(non_existing_file)
-{}
+{
+    char data[] = "data to be written.";
+    std::size_t dataSize = strlen(data);
+    AsyncFileOut outputFile(datasetPath() + "non_extisting_file.txt", WRITE_TRUNC_M);
+    error_code invalidFileErrorCode(errc::no_such_file_or_directory, system_category());
+
+    WriteHandler wHandler = boost::bind(&assertWrite, boost::asio::placeholders::error,
+                                        boost::asio::placeholders::bytes_transferred,
+                                        0, invalidFileErrorCode, true);
+
+    BOOST_REQUIRE(!outputFile.isOpen());
+    boost::asio::const_buffers_1 buf(data, dataSize);
+    outputFile.asyncWrite(buf, wHandler);
+
+    pause(1);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -206,17 +292,34 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(afio_mixed_rw_test)
 
-BOOST_AUTO_TEST_CASE(successful)
-{}
+BOOST_AUTO_TEST_CASE(read_write)
+{
+    AsyncFileInOut reviewFile(datasetPath() + "no_country_for_old_men.txt", READ_WRITE_M);
+    
+    std::string dataToWrite("\n\nIt's one hell of a movie!!");
+    boost::asio::const_buffers_1 writeBuf = boost::asio::buffer(dataToWrite);
+    
+    std::vector<char> readBuf(512);
+    std::size_t readSize = 256;
 
-BOOST_AUTO_TEST_SUITE_END()
+    error_code successErrorCode;
 
-///////////////////////////////////////////////////////////////////////////////
+    ReadHandler rHandler = boost::bind(&assertRead, boost::asio::placeholders::error,
+                                       boost::asio::placeholders::bytes_transferred,
+                                       boost::ref(readBuf), readSize, 
+                                       successErrorCode, true);
+    
+    WriteHandler wHandler = boost::bind(&assertWrite, boost::asio::placeholders::error,
+                                        boost::asio::placeholders::bytes_transferred,
+                                        dataToWrite.size(), successErrorCode, true);
 
-BOOST_AUTO_TEST_SUITE(afio_simultaneous_read_write_test)
+    BOOST_REQUIRE(reviewFile.isOpen());
+    reviewFile.asyncRead(boost::asio::buffer(readBuf), rHandler, readSize);
+    reviewFile.asyncWrite(writeBuf, wHandler);
 
-BOOST_AUTO_TEST_CASE(multiple_read_write)
-{}
+    pause(1);
+    reviewFile.close();
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
